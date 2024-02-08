a25c2852b04d1ada5093cc582908b7137f70d5f2
unknown <sujonahmed45a4@gmail.com>
2024-02-08 18:43:35 +0600

    project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todos from "./components/Todos";
+
+const App = () => {
+  return (
+    <div className="bg-[#14161B] min-h-svh w-full p-20">
+      <Todos />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,5 @@
+const Todos = () => {
+  return <div className="w-2/3 mx-auto bg-[#1C1D24] p-10">Todo</div>;
+};
+
+export default Todos;
